85f3bd62a405e5fe005191e632cca9c3dd8d5ff6
AsangiSathsarani <38518830+AsangiSathsarani@users.noreply.github.com>
2021-05-22 19:58:54 +0530

    Added Raleway font for welcome section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React  from 'react';
 import './App.scss';
 import Library from './view/Library';
-
+import {Col, Container, Row} from 'react-bootstrap';
 const App :React.FC = () =>{
   return(
-      <Library/>
-  )
+      <Container className = 'px-0' fluid>
+        <Row>
+          <Col>
+            <Library/>
+          </Col>
+        </Row>
+      </Container>
+
+  );
 }
 
 export default App;
---
+++ src/Component/Authors/DeleteAuthor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeleteAuthor : React.FC = () =>{
+    return(
+        <h1>
+
+        </h1>
+    );
+}
+
+/**export default DeleteAuthor;**/
