9723ec34bbd73844821c630f0efe8fdae3d83af3
AsangiSathsarani <38518830+AsangiSathsarani@users.noreply.github.com>
2021-05-24 14:28:50 +0530

    Add Author list and Book list with hardcord value

--- src/Component/Books/Book.tsx
+++ src/Component/Books/Book.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-
+import {Row,Col,Container} from "react-bootstrap";
+import { Trash2, Edit } from 'react-feather';
 const Book : React.FC = () =>{
     return(
-        <h1>
+        <Container fluid>
+            <Row className = "Books">
+                <Col xs = {12}>
+                    <h3 className= "title pt-2 pb-2">Books</h3>
 
-        </h1>
+                </Col>
+                <Col xs = {12}>
+                    <ul className= 'book-list px-0'>
+                        <li>
+                            <Row className= 'pb-2 pt-2'>
+                                <Col xs = {10} >1.Books Name</Col>
+                                <Col xs = {1}><Edit className = 'edit-btn'/></Col>
+                                <Col xs = {1}><Trash2 className = 'delete-btn'/></Col>
+                            </Row>
+                        </li>
+                        <li>
+                            <Row className= 'pb-2 pt-2'>
+                                <Col xs = {10} >2.Books Name</Col>
+                                <Col xs = {1}><Edit className = 'edit-btn'/></Col>
+                                <Col xs = {1}><Trash2 className = 'delete-btn'/></Col>
+                            </Row>
+                        </li>
+                        <li>
+                            <Row className= 'pb-2 pt-2'>
+                                <Col xs = {10} >3.Books Name</Col>
+                                <Col xs = {1}><Edit className = 'edit-btn'/></Col>
+                                <Col xs = {1}><Trash2 className = 'delete-btn'/></Col>
+                            </Row>
+                        </li>
+
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+
     );
 }
 
