d8dc68cf2e4a3a2e52080d2c87c835c79dd2e5bd
AsangiSathsarani <38518830+AsangiSathsarani@users.noreply.github.com>
2021-05-24 14:28:50 +0530

    Add Author list and Book list with hardcord value

--- src/Component/Authors/Author.tsx
+++ src/Component/Authors/Author.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
+import {Edit, Trash2} from "react-feather";
 
 const Author : React.FC = () =>{
     return(
-        <h1>
+        <Container fluid>
+            <Row className = "Authors">
+                <Col xs = {12}>
+                    <h3 className= "title pt-2 pb-2">Authors</h3>
 
-        </h1>
+                </Col>
+                <Col xs = {12}>
+                    <ul className= 'author-list px-0'>
+                        <li>
+                            <Row className= 'pb-2 pt-2'>
+                                <Col xs = {10} >1.Author Name</Col>
+                                <Col xs = {1}><Edit className = 'edit-btn'/></Col>
+                                <Col xs = {1}><Trash2 className = 'delete-btn'/></Col>
+                            </Row>
+                        </li>
+                        <li>
+                            <Row className= 'pb-2 pt-2'>
+                                <Col xs = {10} >2.Author Name</Col>
+                                <Col xs = {1}><Edit className = 'edit-btn'/></Col>
+                                <Col xs = {1}><Trash2 className = 'delete-btn'/></Col>
+                            </Row>
+                        </li>
+                        <li>
+                            <Row className= 'pb-2 pt-2'>
+                                <Col xs = {10} >3.Author Name</Col>
+                                <Col xs = {1}><Edit className = 'edit-btn'/></Col>
+                                <Col xs = {1}><Trash2 className = 'delete-btn'/></Col>
+                            </Row>
+                        </li>
+
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
